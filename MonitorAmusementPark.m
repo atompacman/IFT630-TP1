3379461d96263993c9cd6358d4a37ce60b0f515a
alexblouin <alex.blouin64@gmail.com>
2016-02-16 22:25:51 -0500

    [Monitor][Rollercoaster] They don't want to get out :(

--- MonitorAmusementPark.m
+++ MonitorAmusementPark.m
@@ -8,16 +8,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public _monitor MonitorAmusementPark {
 
     // Lap duration in seconds (5 laps = 1 ride)
-    private static final int LAP_DURATION_SECONDS = 1;
+    private static final int LAP_DURATION_SECONDS = 2;
 
     // Program duration in seconds
-    private static final int PROGRAM_DURATION_SECONDS = 30;
+    private static final int PROGRAM_DURATION_SECONDS = 25;
 
     // Maximum time in milliseconds that is randomly added to base time when riding the rollecoaster
     private static final int RANDOM_ADDITIONNAL_TIME_MILLIS = 5;
 
     // Rollercoaster maximum capacity
-    private static final int C = 16;
+    private static final int C = 10;
 
     // Total number of people in line and on the rollercoaster
     private static final int N = 50;
@@ -43,16 +43,10 @@ public _monitor MonitorAmusementPark {
     // Rollercoaster process
     private static process RollerCoaster {
         // It goes on and on
-        monitor.openTheGates();
         while (true) {
-            System.out.println("5 laps and stop");
+            // System.out.println("5 laps and stop");
             monitor.do5LapsAndStop();
         }
-    }
-
-    // Should only be done once, in order to let people in at first
-    private void openTheGates() {
-        _signal(canEnter);
     }
 
     // Person process
@@ -60,38 +54,35 @@ public _monitor MonitorAmusementPark {
         // Those guys never stop !
         
         while (true) {
-            System.out.println("Get in car if possible");
+            // System.out.println("Get in car if possible");
             monitor.getInCarIfPossible(id);
-            System.out.println("Get out of car if possible");
+            // System.out.println("Get out of car if possible");
             monitor.getOutOfCarIfPossible(id);
         }
     }
 
     _proc void getInCarIfPossible(int id) {
-        System.out.println("I'm trying to get in the car!");
-        _wait(canEnter);
+        // System.out.println("I'm trying to get in the car!");
+        if (car.size() == C) {
+            _wait(canEnter);
+        }
         car.add(id);
         System.out.println("Person #" + id + " got in the car. Size = " + car.size());
         if (car.size() == C) {
             System.out.println("Car is now full. The ride is about to begin!");
             _signal(canStart);
         }
-        else
-        {
-            _signal(canEnter);
-        }
     }
 
     _proc void getOutOfCarIfPossible(int id) {
         _wait(canExit);
-        car.remove(new Integer(id));    // 
+        System.out.println("Trying to get out!");
+
+        car.remove(new Integer(id));
         System.out.println("Person #" + id + " got out of the car. Size = " + car.size());
         if (car.size() == 0) {
             System.out.println("Car is now empty. People will now be able to get in.");
             _signal(canEnter);
-        }
-        else {
-            _signal(canExit);
         }
     }
 
